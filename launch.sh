c55ffec44eacccaca09cab3cee94ad3816e2c903
Nelson Neves <nelson.s.neves@gmail.com>
2013-09-22 01:48:38 +0100

    Add OpenWRT node.js demo launch scripts

---
+++ launch.sh
@@ -0,0 +1,3 @@
+# adjust serialport and speed in ./config/linux.json
+export NODE_ENV=linux
+node --stack_size=1024 --max_old_space_size=20 --max_new_space_size=2048 --max_executable_size=5 --gc_global --gc_interval=100 app.js ./bin/gcode/smallwheel.gcode
